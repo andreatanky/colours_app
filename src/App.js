fd241eee4b6234e25e7896c85728e67fe80d14a3
Andrea Tan <andreaxtan611@gmail.com>
2021-01-04 02:36:14 +0800

    Style mini palette

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
 import Palette from './Palette';
 import seedColors from './seedColors';
 import {generatePalette} from "./colorHelpers";
-import {Route, Router, Switch} from 'react-router-dom';
+import {Route, Switch} from 'react-router-dom';
+import PaletteList from './PaletteList';
 
-function App() {
-  return (
-    <Switch>
-      <Route exact path="/" render={() => <h1>PALETTE LIST GOES HERE</h1>}/>
-      <Route exact path="/palette/:id" render={() => <h1>INDIV LIST GOES HERE</h1>}/>
-    </Switch>
-    // <div className="App">
-    //   <Palette palette={generatePalette(seedColors[4])}/>
-    // </div>
-  );
+
+
+class App extends Component {
+
+  findPalette(id) {
+    return seedColors.find(function(palette) {
+      return palette.id === id;
+    })
+  }
+
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/" render={() => <PaletteList palettes={seedColors}/>}/>
+        <Route exact path="/palette/:id" render={routeProps => <Palette palette={generatePalette(this.findPalette(routeProps.match.params.id))}/>}/>
+      </Switch>
+      // <div className="App">
+      //   <Palette palette={generatePalette(seedColors[4])}/>
+      // </div>
+    );
+  }
 }
 
 export default App;
